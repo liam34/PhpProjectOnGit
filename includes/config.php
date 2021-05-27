711c091e5c588153ac9b2662034074b072b360cb
liam34 <indemandcoding@gmail.com>
2021-05-26 19:35:16 -0600

    edit config.php

--- includes/config.php
+++ includes/config.php
@@ -1,8 +1,8 @@
 <?php
-    session_start();
+    
     
     ob_start();
-
+    session_start();
     $timezone = date_default_timezone_set("America/Edmonton");
 
     $con = mysqli_connect("localhost", "root", "", "phpprojectdb1.2");

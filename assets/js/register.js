54aa162a8bce244639c4a497f72ea5c6981b10db
liam34 <indemandcoding@gmail.com>
2021-05-27 23:54:58 -0600

    debug js

--- assets/js/register.js
+++ assets/js/register.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
+    console.log("document is ready");
     $("#hideLogin").click(function(){
-        $(".loginForm").hide();
-        $(".#registerForm").show();
+        $("#loginForm").hide();
+        $("#registerForm").show();
             })
             $("#hideRegister").click(function(){
                 $("#loginForm").show();
--- register.php
+++ register.php
@@ -25,9 +25,12 @@ function getInputValue($name) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome to Slotify</title>
+    
     <link rel="stylesheet" type="text/css" href="assets/css/register.css">
+
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 
+    <script src="assets/js/register.js"></script>
 </head>
 
 <body>

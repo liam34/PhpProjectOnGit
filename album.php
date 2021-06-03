063cc3a8d5584475fda9ac6e1a7af4671673755f
liam34 <indemandcoding@gmail.com>
2021-06-02 22:45:18 -0600

    commit

--- album.php
+++ album.php
@@ -11,11 +11,14 @@ else {
 $albumQuery = mysqli_query($con, "SELECT * FROM albums WHERE id='$albumId'");
 $album = mysqli_fetch_array($albumQuery);
 
-echo $album['title'];
+$artistId = $album['artist'];
+
+$artistQuery = mysqli_query($con, "SELECT * FROM artists WHERE id='$artistId'");
+$artist = mysqli_fetch_array($artistQuery);
+
+echo $artist['name'];
 
 ?>
-
-
 
 
 <?php include("includes/footer.php");?>
